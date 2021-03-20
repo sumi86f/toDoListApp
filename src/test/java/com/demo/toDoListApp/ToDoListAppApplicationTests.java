155e1c1eeab43657ffd1acf6c39358877a02bff3
sumi86f <80991636+sumi86f@users.noreply.github.com>
2021-03-20 11:46:12 +1000

    Initial commit

---
+++ src/test/java/com/demo/toDoListApp/ToDoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.toDoListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
